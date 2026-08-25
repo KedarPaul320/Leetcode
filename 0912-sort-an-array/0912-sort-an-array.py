18a2309ce191d282310bf98502103b1990502cd8
KEDAR PAUL <kedarpaul53@gmail.com>
2026-08-25 22:12:22 +0530

    Time: 640 ms (51.26%), Space: 29.4 MB (15.9%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,42 +1,32 @@
 class Solution:
-    def merge(self,nums,left,mid,right):
-        a = []
-        b = []
-
-        for i in range(left,mid+1):
-            a.append(nums[i])
-        for i in range(mid+1,right+1):
-            b.append(nums[i])
-
-        i,j,k = 0 , 0, left
-        while k<=right :
-            if j == len(b):
-                nums[k] = a[i]
-                i+=1
-                k+=1
-            elif i == len(a):
-                nums[k] = b[j]
-                j+=1
-                k+=1
-            elif a[i]<b[j]:
-                nums[k] = a[i]
-                i+=1
-                k+=1
-            else :
-                nums[k] = b[j]
-                j+=1
-                k+=1
-
-    def mergesort(self,nums,left,right):
-        if left >= right :
-            return 
+    def mergesort(self, nums):
+        if len(nums) <= 1:
+            return nums
         
-        mid = (left + right)//2
+        mid = len(nums) // 2
+        leftHalf = nums[:mid]
+        rightHalf = nums[mid:]
         
-        self.mergesort(nums,left,mid)
-        self.mergesort(nums,mid+1,right)
+        sortedLeft = self.mergesort(leftHalf)
+        sortedRight = self.mergesort(rightHalf)
+        
+        return self.merge(sortedLeft, sortedRight)
+        
+    def merge(self, left, right):
+        result = []
+        i = j = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+                
+        result.extend(left[i:])
+        result.extend(right[j:])
+        return result
 
-        self.merge(nums,left,mid,right)
     def sortArray(self, nums: List[int]) -> List[int]:
-        self.mergesort(nums,0,len(nums)-1)
-        return nums
+        return self.mergesort(nums)
